27c17c08437007bab254944355b42097c52bf74f
SakshamBansal753 <kanhabansal563@gmail.com>
2026-05-17 00:57:46 +0530

    Create abot page

--- cocktail/src/App.jsx
+++ cocktail/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import gsap from 'gsap'
 import {ScrollTrigger,SplitText} from 'gsap/all'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Cocktails from './components/Cocktails'
+import About from './components/About'
 gsap.registerPlugin(ScrollTrigger,SplitText) //make this plugins  to use globally
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
      <Navbar/>
      <Hero/>
     <Cocktails/>
+    <About/>
    </main>
   )
 }
